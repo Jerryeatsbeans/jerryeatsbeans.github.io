1fcf449390a8ddb58e08f874704d00443d75d362
Jerryeatsbeans <Jerryeatsbeans@gmail.com>
2025-04-22 21:36:52 -0400

    Final Commit

--- STLP/scripts/script.js
+++ STLP/scripts/script.js
@@ -2,8 +2,21 @@ function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
 
-document.getElementById('join-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Welcome to the Crimson Knights!');
-});
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.carousel-image');
+    let currentIndex = 0;
+    const slideInterval = 4000;
+    images[currentIndex].classList.add('active');
+    setInterval(() => {
+
+
+      images[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].classList.add('active');
+
+
+    }, slideInterval);
+  })
